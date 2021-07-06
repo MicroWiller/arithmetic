d0a731a3951dbfb1b079bba169c76f148a58fc17
will <will@tencent.com>
2021-07-06 20:37:31 +0800

    add leetcode 23

--- list/gene_new_list.go
+++ list/gene_new_list.go
@@ -177,3 +177,82 @@ func mergeTwoLists(l1, l2 *ListNode) *ListNode {
 	tail.Next = nil
 	return dummy.Next
 }
+
+/*
+ * [23] 合并K个升序链表
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * 示例 1：
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * ⁠ 1->4->5,
+ * ⁠ 1->3->4,
+ * ⁠ 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ *
+ * 示例 2：
+ * 输入：lists = []
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：lists = [[]]
+ * 输出：[]
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	heap := new(nodeHeap)
+	for i := 0; i < len(lists); i++ {
+		if lists[i] != nil {
+			heap.Push(lists[i])
+		}
+	}
+
+	dummy := &ListNode{
+		Value: 0,
+		Next:  nil,
+	}
+	tail := dummy
+
+	for heap.Len() > 0 {
+		cur := heap.Pop().(ListNode)
+		tail.Next = &cur
+		tail = &cur
+
+		if cur.Next != nil {
+			heap.Push(cur.Next)
+		}
+	}
+	tail.Next = nil
+	return dummy.Next
+}
+
+type nodeHeap []ListNode
+
+// Less 小根堆
+func (h *nodeHeap) Less(i, j int) bool {
+	return (*h)[i].Value < (*h)[j].Value
+}
+
+func (h *nodeHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *nodeHeap) Len() int {
+	return len(*h)
+}
+
+func (h *nodeHeap) Pop() (v interface{}) {
+	*h, v = (*h)[:h.Len()-1], (*h)[h.Len()-1]
+	return
+}
+
+func (h *nodeHeap) Push(v interface{}) {
+	*h = append(*h, v.(ListNode))
+}
